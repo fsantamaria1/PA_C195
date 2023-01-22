810ac9ad9d73f6b3ab7f6d9ce93fff67140647c7
Fredy Santamaria <fsanta3@wgu.edu>
2023-01-21 23:46:26 -0500

    Added the mysql-connector Maven library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module santamaria.pa_c195 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.java;
+//    requires mysql.connector.java;
 
 
     opens santamaria.pa_c195 to javafx.fxml;
